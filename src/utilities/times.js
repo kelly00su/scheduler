e88a87c893fafbab9b58b7b118814c6d1cbcdc2e
kelly00su <56286184+kelly00su@users.noreply.github.com>
2022-01-08 11:00:43 -0600

    write to firebase

--- src/utilities/times.js
+++ src/utilities/times.js
@@ -1,7 +1,7 @@
 // parse meeting strings 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
-const timeParts = meets => {
+export const timeParts = meets => {
   const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
   return !match ? {} : {
     days,
